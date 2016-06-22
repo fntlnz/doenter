707d7aa5b8d761012954c31abf0838aa7979690b
Lorenzo Fontana <fontanalorenzo@me.com>
2016-06-22 20:32:42 +0200

    Initial signal handling

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/tarm/serial"
 )
 
 func main() {
-
 	// read
 	go func() {
 		c := &serial.Config{Name: "/dev/ttys001", Baud: 9600}
@@ -47,7 +47,28 @@ func main() {
 		}
 	}()
 
+	// Write signals
+	go func() {
+		c := &serial.Config{Name: "/dev/ttys001", Baud: 9600}
+		s, err := serial.OpenPort(c)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, os.Interrupt)
+		for sig := range sigchan {
+			switch sig {
+			case os.Interrupt:
+				// TODO(fntlnz): I don't know how to send signals to the tty atm, blame on me
+				break
+
+			}
+		}
+	}()
+
 	for {
 
 	}
+
 }
